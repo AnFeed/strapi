976fbf0e539ece9aa11e2ad33d9908b7e4188ebe
Atesz23 <anfeed@anfeed.ro>
2024-06-05 15:51:42 +0300

    services controller changed

--- src/api/service/controllers/service.js
+++ src/api/service/controllers/service.js
@@ -1,9 +1,21 @@
-'use strict';
+"use strict";
 
 /**
- * service controller
+ * blog controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::service.service');
+module.exports = createCoreController("api::service.service", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.db.query("api::service.service").findOne({
+      where: { slug: id },
+      populate: ["deep"],
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
